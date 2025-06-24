db288e4525c799c3f0f72be2db26824d39e2f0eb
Enrique Bruzual <bruzual.kike@gmail.com>
2025-06-23 21:07:34 -0400

    Enhance server configuration to include host and port settings

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from mcp.server.fastmcp import FastMCP
+import os
 
 mcp = FastMCP("HTTP Server", "0.1.0")
 
@@ -12,4 +13,4 @@ def greeting(name: str) -> str:
 
 
 if __name__ == "__main__":
-    mcp.run(transport="streamable-http") # type: ignore
+    mcp.run(transport="streamable-http", host="0.0.0.0", port=int(os.environ.get("PORT", 8000))) # type: ignore
